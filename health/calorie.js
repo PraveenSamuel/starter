176a7509d3740ef255f33535d854115c999d80f9
Bharadwaj1431996 <bharadwajdaya@gmail.com>
2018-03-10 13:35:17 +0530

    New Folder Changes

--- health/calorie.js
+++ health/calorie.js
@@ -12,3 +12,28 @@ function setGender(val)
 		sessionStorage.setItem("gender","male");
 	}
 }
+function calImpunitChange(event,id)
+{
+	var oldunit=$("#"+id).html().split('<')[0];
+	var hOrW;
+	if(id=="bmi_imp_hei_drp")
+	{
+		hOrW='h';
+	}
+	else
+	{
+		hOrW='w';
+	}
+	var current_unit=event.target.innerHTML;//li clicked element value 
+	var value=inputValueCorresToDrpDwn(id);//input box value
+	$("#"+id).html(currentUnitWithoutBraces(current_unit));
+	var newvalue=OldUnitToCurrentUnit(hOrW,oldunit,$("#"+id).html(),value);
+	if(id=="bmi_imp_hei_drp")
+	{
+		$("[purpose=cal_imp_height]").val(newvalue);
+	}
+	else
+	{
+		$("[purpose=cal_imp_weight]").val(newvalue);
+	}
+}
---
+++ health/health_menu.js
@@ -0,0 +1,6 @@
+$('[purpose="BMI"]').click(function(){
+	window.location.href="bmi_calc.html";
+});
+$('[purpose="calorie"]').click(function(){
+	window.location.href="calorie_calc.html";
+});
